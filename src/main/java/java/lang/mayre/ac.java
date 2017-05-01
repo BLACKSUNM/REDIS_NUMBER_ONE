b895b0253b504d5e0e2edd7bb4460b8b18125a4f
hackerprince <791427172@qq.com>
2017-05-01 19:34:30 +0800

    ch1

---
+++ src/main/java/java/lang/mayre/ac.java
@@ -0,0 +1,13 @@
+package java.lang.mayre;
+
+/**
+ * Hello world!
+ *
+ */
+public class ac
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
