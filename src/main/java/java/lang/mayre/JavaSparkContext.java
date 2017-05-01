340feee367bd0ae5d19be124ed5db5d6ff8d7654
hackerprince <791427172@qq.com>
2017-05-01 19:37:22 +0800

    ch2

---
+++ src/main/java/java/lang/mayre/JavaSparkContext.java
@@ -0,0 +1,15 @@
+package java.lang.mayre;
+
+import jdk.nashorn.internal.runtime.regexp.joni.Config;
+
+/**
+ * Created by BlackSun on 2017/5/1.
+ */
+public class JavaSparkContext {
+    private String name;
+    private int age;
+
+    public JavaSparkContext(String name) {
+        this.name = name;
+    }
+}
